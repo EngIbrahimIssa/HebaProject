a0150a3444298dc1d3739b243381357fdb94aaab
ibr <01ibrahim.issa@gmail.com>
2023-08-22 15:03:14 +0300

    first version

---
+++ app/src/main/java/com/example/finalproject/SplashScreen.java
@@ -0,0 +1,39 @@
+package com.example.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    ImageView img_icon;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen2);
+
+        img_icon = findViewById(R.id.img_icon);
+
+        Animation animUpDown;
+        animUpDown = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.up_down);
+        img_icon.startAnimation(animUpDown);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+            }
+        }, 3000);
+    }
+}
